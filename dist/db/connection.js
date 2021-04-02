ff2f7963205d2f07007fe96dab8471d03e3dcd7c
Oscar Montoya <oscar.montoya@puntoscolombia.com>
2021-04-01 21:39:18 -0500

    borrar directorio dist

--- dist/db/connection.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize('node', 'demos', '123456', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
-exports.default = db;
-//# sourceMappingURL=connection.js.map
