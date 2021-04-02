aec1dd78b0eb9cfa4434022eed3cc6dca1874505
Oscar Montoya <oscar.montoya@puntoscolombia.com>
2021-04-01 21:34:52 -0500

    codigo de fernando

--- app.ts
+++ app.ts
@@ -1,9 +1,10 @@
 import dotenv from 'dotenv';
 import Server from './models/server';
 
-// configurar dotenv
+// Configurar dot.env
 dotenv.config();
 
 const server = new Server();
+
 
 server.listen();
