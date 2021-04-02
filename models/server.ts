da22dc5872a380c93bca550d854436d8b7e4cded
Oscar Montoya <oscar.montoya@puntoscolombia.com>
2021-04-01 22:38:01 -0500

    funcionando hasta video 5

--- models/server.ts
+++ models/server.ts
@@ -2,7 +2,6 @@ import express, { Application } from 'express';
 import userRoutes from '../routes/usuario';
 import cors from 'cors';
 
-import db from '../db/connection';
 
 
 class Server {
@@ -17,47 +16,29 @@ class Server {
         this.app  = express();
         this.port = process.env.PORT || '8000';
 
-        // Métodos iniciales
-        this.dbConnection();
-        this.middlewares();
+        
+        // Metodos iniciales
+        this.middleware();
         this.routes();
-    }
-
-    async dbConnection() {
-
-        try {
-            
-            await db.authenticate();
-            console.log('Database online');
-
-        } catch (error) {
-            throw new Error( error );
-        }
-
-    }
-
-    middlewares() {
-
-        // CORS
-        this.app.use( cors() );
-
-        // Lectura del body
-        this.app.use( express.json() );
-
-        // Carpeta pública
-        this.app.use( express.static('public') );
-    }
-
-
-    routes() {
-        this.app.use( this.apiPaths.usuarios, userRoutes )
     }
 
 
     listen() {
         this.app.listen( this.port, () => {
-            console.log('Servidor corriendo en puerto ' + this.port );
+            console.log('Servidor corriendo en puerto !!!' + this.port );
         })
+    }
+
+    routes() {
+        this.app.use(this.apiPaths.usuarios, userRoutes)
+    }
+
+    middleware() {
+        // CORS
+        this.app.use(cors());
+        // lectura del body
+        this.app.use(express.json());
+        //carpeta publica
     }
 
 }
